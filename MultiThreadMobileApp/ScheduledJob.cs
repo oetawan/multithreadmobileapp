71ba80b6940446432c8b32c029bc4f7b56859a49
oetawan <oetawan@gmail.com>
2013-06-08 06:42:22 +0700

    Added scheduledjob abstract class

---
+++ MultiThreadMobileApp/ScheduledJob.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiThreadMobileApp
+{
+    public abstract class ScheduledJob: IScheduledJob
+    {
+        int _repeatIntervalInMinutes;
+        public ScheduledJob(DateTime date, int repeatIntervalInMinutes)
+        {
+            Time = date;
+            _repeatIntervalInMinutes = repeatIntervalInMinutes;
+        }
+
+        #region IScheduledJob Members
+
+        public DateTime Time { get; private set; }
+
+        #endregion
+
+        #region IJob Members
+
+        public void Execute()
+        {
+            DoExecute();
+            Time = DateTime.Now.AddMinutes(_repeatIntervalInMinutes);
+        }
+
+        #endregion
+
+        protected abstract void DoExecute();
+    }
+}
